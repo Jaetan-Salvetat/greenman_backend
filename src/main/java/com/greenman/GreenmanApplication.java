d70e87f3b3e8a8a46fc4a3a8eb7a96e461638f93
gaetan16 <gaetan@stock-pro.fr>
2023-01-02 14:10:51 +0100

    init java project

---
+++ src/main/java/com/greenman/GreenmanApplication.java
@@ -0,0 +1,13 @@
+package com.greenman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenmanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GreenmanApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/greenman/GreenmanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greenman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreenmanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
